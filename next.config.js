533e1ce1458a7b5b6f24ee0a454ceee0a5f07bfe
Felix Mattick <felix.mattick@gmail.com>
2019-02-15 18:47:09 -0500

    Possibly add servcie worker

--- next.config.js
+++ next.config.js
@@ -1,3 +1,27 @@
-module.exports = {
-  target: 'serverless'
+const withOffline = require('next-offline')
+
+const config = {
+  target: 'serverless',
+  workboxOpts: {
+    swDest: 'static/service-worker.js',
+    runtimeCaching: [
+      {
+        urlPattern: /^https?.*/,
+        handler: 'networkFirst',
+        options: {
+          cacheName: 'https-calls',
+          networkTimeoutSeconds: 15,
+          expiration: {
+            maxEntries: 150,
+            maxAgeSeconds: 30 * 24 * 60 * 60,
+          },
+          cacheableResponse: {
+            statuses: [ 0, 200 ]
+          }
+        }
+      }
+    ]
+  }
 }
+
+module.exports = withOffline(config)
