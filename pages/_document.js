a8f95b494937ca91701aa244d709b5f7f6ae3082
Felix Mattick <felix.mattick@gmail.com>
2019-02-15 20:32:58 -0500

    Add opengraph tags

--- pages/_document.js
+++ pages/_document.js
@@ -7,6 +7,12 @@ export default class extends Document {
         <Head>
           <meta name='viewport' content='width=device-width, initial-scale=1' />
           <meta name='theme-color' content='#51cbee' />
+          <meta name='description' content='Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur finibus dapibus odio at sodales. Suspendisse sit amet fringilla ex.' />
+          <meta property='og:description' content='Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur finibus dapibus odio at sodales. Suspendisse sit amet fringilla ex.' />
+          <meta property='og:title' content='Caspar Lant' />
+          <meta property='og:site_name' content='Caspar.cc' />
+          <meta property='og:type' content='website' />
+          <title>Caspar Lant</title>
         </Head>
         <body>
           <Main />
